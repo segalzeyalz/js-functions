0cfd576ed5719faa9c92fc6d0983b3f11863e01b
segalzeyalz <segaleseo@gmail.com>
2019-03-01 15:15:32 +0200

    add a new function

--- numFuncs.js
+++ numFuncs.js
@@ -1,6 +1,18 @@
 let numFuncs =  (() =>{ return {
     isInteger: (a) =>{
         return a%1===0;
+    },
+    biggestDivider: (a,b)=>{
+        let temp
+        if(a>b){
+            if(a%b===0){
+                return b;
+            }
+        } else{
+            if(b%a===0){
+                return a;
+            }
+        }
     }
 }
 })()
